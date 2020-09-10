30f4857fa4eebce8881e927408cb82b54ed6c9ce
Sinclair Liang <sinclairliang@gmail.com>
2020-09-10 01:28:31 -0500

    first commit

---
+++ src/leetcode/TwoSum.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * You can return the answer in any order.
+ */
+public class TwoSum {
+}
