38d809810deaa78e009b6b14ad97d0c94a3b0e4f
Sinclair Liang <sinclairliang@gmail.com>
2020-09-14 17:48:54 -0500

    Added AtoI

---
+++ src/leetcode/StringtoIntegerAtoi.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+/**
+ * Implement atoi which converts a string to an integer.
+ * <p>
+ * The function first discards as many whitespace characters as necessary until the first non-whitespace character
+ * is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many
+ * numerical digits as possible, and interprets them as a numerical value.
+ * <p>
+ * The string can contain additional characters after those that form the integral number, which are ignored and
+ * have no effect on the behavior of this function.
+ * <p>
+ * If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence
+ * exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+ * <p>
+ * If no valid conversion could be performed, a zero value is returned.
+ * <p>
+ * Note:
+ * <p>
+ * Only the space character ' ' is considered as whitespace character.
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer
+ * range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1)
+ * or INT_MIN (−231) is returned.
+ */
+public class StringtoIntegerAtoi {
+    /**
+     * @param str
+     * @return
+     */
+    public int myAtoi(String str) {
+        if (str == null) {
+            return 0;
+        }
+        str = str.trim();
+        if (str.length() == 0) {
+            return 0;
+        }
+        char firstChar = str.charAt(0);
+        int sign = 1;
+        int start = 0;
+        long res = 0;
+
+        if (firstChar == '+') {
+            start++;
+        } else if (firstChar == '-') {
+            sign = -1;
+            start++;
+        }
+        for (int i = start; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return (int) res * sign;
+            }
+            res = res * 10 + str.charAt(i) - '0';
+            if (sign == 1 && res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (sign == -1 && res > Integer.MAX_VALUE) return Integer.MIN_VALUE;
+
+        }
+        return (int) (sign * res);
+    }
+}
