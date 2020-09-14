5a670dd86826a0e01c7096f01a87e3e8cce5c87f
Sinclair Liang <sinclairliang@gmail.com>
2020-09-14 01:32:18 -0500

    added Longest Palindromic Substring

---
+++ src/leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example 2:
+ * <p>
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
