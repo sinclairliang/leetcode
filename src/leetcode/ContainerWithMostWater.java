df0d1bd33e146a2d958b11d3b487c64ef9ba71a8
Sinclair Liang <sinclairliang@gmail.com>
2020-09-22 16:40:40 -0500

    working  on No.11

---
+++ src/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        return 0;
+    }
+}
