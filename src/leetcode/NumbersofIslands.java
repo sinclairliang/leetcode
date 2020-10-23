90da71a15ed3b620119d9000fc2d2c0480414afe
Sinclair Liang <sinclairliang@gmail.com>
2020-10-22 21:28:27 -0500

    Number of Islands

---
+++ src/leetcode/NumbersofIslands.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class NumbersofIslands {
+
+    public int numIslands(char[][] grid) {
+        return 0;
+    }
+
+    private void bfs(char[][] grid, int i, int j) {
+
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+
+    }
+}
